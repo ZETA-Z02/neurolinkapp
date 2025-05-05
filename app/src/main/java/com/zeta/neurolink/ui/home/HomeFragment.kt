7fc906e7a8e421bb51ad3ae0e8436fd4c8f22099
zeta <jersson.z032@gmail.com>
2025-05-05 10:50:07 -0500

    ultimos cambios, corregir hombe, que no visualiza datos

--- app/src/main/java/com/zeta/neurolink/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zeta/neurolink/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.zeta.neurolink.databinding.FragmentHomeBinding
@@ -22,15 +23,18 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        //val homeViewModel =
+            //ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        //val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        binding.button.setOnClickListener {
+            Toast.makeText(requireContext(), "Botón presionado", Toast.LENGTH_SHORT).show()
         }
         return root
     }
