69d5f938a502a6e81a1e08681d2250da4b15cb51
zeta <jersson.z032@gmail.com>
2025-05-06 13:01:19 -0500

    FIX(mainactivity):Corregi el error de ingresar a home

--- app/src/main/java/com/zeta/neurolink/MainActivity.kt
+++ app/src/main/java/com/zeta/neurolink/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.zeta.neurolink
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -19,28 +19,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root) // ✅ solo esta línea es necesaria
 
         val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
         // Forzar modo claro
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        setContentView(R.layout.activity_main)
-        //id usuario
-        val idusuario = intent.getIntExtra("idusuario",-1)
 
+        // id usuario
+        val idusuario = intent.getIntExtra("idusuario", -1)
+        Toast.makeText(this, "$idusuario", Toast.LENGTH_LONG).show()
     }
 }
